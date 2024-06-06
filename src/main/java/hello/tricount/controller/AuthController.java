556cea3c635f2754fd227c70e5ed2220b65fb9d1
geunhokinn <gghh1220@gmail.com>
2024-06-06 15:14:29 +0900

    feat: 로그인 컨트롤러 추가

--- src/main/java/hello/tricount/controller/AuthController.java
+++ src/main/java/hello/tricount/controller/AuthController.java
@@ -1,8 +1,12 @@
 package hello.tricount.controller;
 
+import hello.tricount.TricountConst;
+import hello.tricount.model.LoginRequest;
 import hello.tricount.model.Member;
 import hello.tricount.model.SignupRequest;
 import hello.tricount.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +23,7 @@ public class AuthController {
     @PostMapping("signup")
     public ResponseEntity<Member> signup(@RequestBody SignupRequest signupRequest) {
         Member member = Member.builder()
-                .loginId(signupRequest.getUserId())
+                .loginId(signupRequest.getLoginId())
                 .password(signupRequest.getPassword())
                 .name(signupRequest.getName())
                 .build();
@@ -27,4 +31,13 @@ public class AuthController {
         return new ResponseEntity<>(memberService.signup(member), HttpStatus.OK);
     }
 
+    @PostMapping("login")
+    public ResponseEntity<Member> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        Member member = memberService.login(loginRequest.getLoginId(), loginRequest.getPassword());
+
+        Cookie cookie = new Cookie(TricountConst.LOGIN_MEMBER_COOKIE, member.getLoginId());
+        response.addCookie(cookie);
+
+        return new ResponseEntity<>(member, HttpStatus.OK);
+    }
 }
