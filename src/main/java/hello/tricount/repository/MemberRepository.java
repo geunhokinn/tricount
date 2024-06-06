443fb74996067b0a6175abff48bfff04cd64f629
geunhokinn <gghh1220@gmail.com>
2024-06-06 15:14:46 +0900

    feat: 로그인 레파지토리 추가

--- src/main/java/hello/tricount/repository/MemberRepository.java
+++ src/main/java/hello/tricount/repository/MemberRepository.java
@@ -3,12 +3,15 @@ package hello.tricount.repository;
 import hello.tricount.model.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -28,5 +31,21 @@ public class MemberRepository {
         Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(params));
         member.setId(key.longValue());
         return member;
+    }
+
+    public Optional<Member> findByLoginId(String loginId, String password) {
+        List<Member> result = jdbcTemplate.query("SELECT * FROM member WHERE login_id = ? AND password = ?", memberRowMapper(), loginId, password);
+        return result.stream().findFirst();
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setLoginId(rs.getString("login_id"));
+            member.setName(rs.getString("name"));
+            member.setPassword(rs.getString("password"));
+            return member;
+        };
     }
 }
