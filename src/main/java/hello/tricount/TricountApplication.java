47903685a19fc633eb7430102adeda7d53022afe
geunhokinn <gghh1220@gmail.com>
2024-06-06 13:44:20 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/hello/tricount/TricountApplication.java
@@ -0,0 +1,13 @@
+package hello.tricount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TricountApplication.class, args);
+    }
+
+}
