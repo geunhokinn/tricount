997c8a4c668db07ee2dd1e232eb47098254c9048
geunhokinn <gghh1220@gmail.com>
2024-06-10 23:55:07 +0900

    feat: 정산 내역 조회 레파지토리 추가

--- src/main/java/hello/tricount/repository/SettlementRepository.java
+++ src/main/java/hello/tricount/repository/SettlementRepository.java
@@ -1,5 +1,6 @@
 package hello.tricount.repository;
 
+import hello.tricount.model.ExpenseResult;
 import hello.tricount.model.Member;
 import hello.tricount.model.Settlement;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -69,5 +71,36 @@ public class SettlementRepository {
             settlement.setParticipants(participants);
             return settlement;
         });
+    }
+
+    public List<ExpenseResult> findExpensesWithMemberBySettlementId(Long settlementId) {
+        String sql = "SELECT * " +
+                "FROM settlement_participant " +
+                "JOIN member ON settlement_participant.member_id = member.id " +
+                "LEFT JOIN expense ON settlement_participant.member_id = expense.payer_member_id " +
+                "AND settlement_participant.settlement_id = expense.settlement_id " +
+                "WHERE settlement_participant.settlement_id = ?";
+        return jdbcTemplate.query(sql, expenseResultRowMapper(), settlementId);
+    }
+
+    private RowMapper<ExpenseResult> expenseResultRowMapper() {
+        return (rs, rowNum) -> {
+            ExpenseResult expenseResult = new ExpenseResult();
+            expenseResult.setSettlementId(rs.getLong("settlement_participant.settlement_id"));
+            BigDecimal amt = rs.getBigDecimal("expense.amount");
+            expenseResult.setAmount(amt != null ? amt : BigDecimal.ZERO);
+
+            Member member = new Member();
+            if(rs.getLong("member.id") != 0) {
+                member.setId(rs.getLong("member.id"));
+                member.setLoginId(rs.getString("member.login_id"));
+                member.setPassword(rs.getString("member.password"));
+                member.setName(rs.getString("member.name"));
+
+                expenseResult.setPayerMember(member);
+            }
+
+            return expenseResult;
+        };
     }
 }
